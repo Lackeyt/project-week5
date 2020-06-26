64617bd5e842c9f6b6fc4817ec99003cde25fd0a
Tyson Lackey <lackeyt90@gmail.com>
2020-06-26 11:13:42 -0700

    refactored tests and business logic

--- _tests_/project-week5.test.js
+++ _tests_/project-week5.test.js
@@ -8,35 +8,35 @@ describe('UserPlanetsAge', () =>{
   })
 
   test("return user inputted age in Mercury years", () => {
-    expect(reusableUserPlanetsAge.mercuryAge()).toEqual(41.67);
+    expect(reusableUserPlanetsAge.mercuryAge(reusableUserPlanetsAge.earthAge)).toEqual(41.67);
   })
 
   test("return user inputted age in Venus years", () => {
-    expect(reusableUserPlanetsAge.venusAge()).toEqual(16.13);
+    expect(reusableUserPlanetsAge.venusAge(reusableUserPlanetsAge.earthAge)).toEqual(16.13);
   })
 
   test("return user inputted age in mars years", () => {
-    expect(reusableUserPlanetsAge.marsAge()).toEqual(5.32);
+    expect(reusableUserPlanetsAge.marsAge(reusableUserPlanetsAge.earthAge)).toEqual(5.32);
   })
 
   test("return user inputted age in Jupiter years", () => {
-    expect(reusableUserPlanetsAge.jupiterAge()).toEqual(.84);
+    expect(reusableUserPlanetsAge.jupiterAge(reusableUserPlanetsAge.earthAge)).toEqual(.84);
   })
 
   test("return user inputted age in saturn years", () => {
-    expect(reusableUserPlanetsAge.saturnAge()).toEqual(.34);
+    expect(reusableUserPlanetsAge.saturnAge(reusableUserPlanetsAge.earthAge)).toEqual(.34);
   })
 
   test("return user inputted age in uranus years", () => {
-    expect(reusableUserPlanetsAge.uranusAge()).toEqual(.12);
+    expect(reusableUserPlanetsAge.uranusAge(reusableUserPlanetsAge.earthAge)).toEqual(.12);
   })
 
   test("return user inputted age in neptune years", () => {
-    expect(reusableUserPlanetsAge.neptuneAge()).toEqual(.06);
+    expect(reusableUserPlanetsAge.neptuneAge(reusableUserPlanetsAge.earthAge)).toEqual(.06);
   })
 
   test("return user inputted age in pluto years", () => {
-    expect(reusableUserPlanetsAge.plutoAge()).toEqual(.04);
+    expect(reusableUserPlanetsAge.plutoAge(reusableUserPlanetsAge.earthAge)).toEqual(.04);
   })
 
 })
--- src/project-week5.js
+++ src/project-week5.js
@@ -3,53 +3,46 @@
 export class UserPlanetsAge{
   constructor(age){
     this.earthAge = age
-    //this.mercuryAge = parseFloat((age / .24).toFixed(2));
-    //this.venusAge = parseFloat((age / .62).toFixed(2));
-    //this.marsAge = parseFloat((age / 1.88).toFixed(2));
-    //this.jupiterAge = parseFloat((age / 11.86).toFixed(2));
-    //this.saturnAge = parseFloat((age / 29.46).toFixed(2));
-    //this.uranusAge = parseFloat((age / 84.01).toFixed(2));
-    //this.neptuneAge = parseFloat((age / 164.79).toFixed(2));
-    //this.plutoAge = parseFloat((age / 248.59).toFixed(2));
+    //this.earthLifeExpectancy
   }
 
-  mercuryAge(){
-    let mercuryAge = parseFloat((this.earthAge / .24).toFixed(2))
+  mercuryAge(years){
+    let mercuryAge = parseFloat((years / .24).toFixed(2))
     return mercuryAge
   }
 
-  venusAge(){
-    let venusAge = parseFloat((this.earthAge / .62).toFixed(2))
+  venusAge(years){
+    let venusAge = parseFloat((years / .62).toFixed(2))
     return venusAge
   }
 
-  marsAge(){
-    let marsAge = parseFloat((this.earthAge / 1.88).toFixed(2))
+  marsAge(years){
+    let marsAge = parseFloat((years / 1.88).toFixed(2))
     return marsAge
   }
 
-  jupiterAge(){
-    let jupiterAge = parseFloat((this.earthAge / 11.86).toFixed(2))
+  jupiterAge(years){
+    let jupiterAge = parseFloat((years / 11.86).toFixed(2))
     return jupiterAge
   }
 
-  saturnAge(){
-    let saturnAge = parseFloat((this.earthAge / 29.46).toFixed(2))
+  saturnAge(years){
+    let saturnAge = parseFloat((years / 29.46).toFixed(2))
     return saturnAge
   }
 
-  uranusAge(){
-    let uranusAge = parseFloat((this.earthAge / 84.01).toFixed(2))
+  uranusAge(years){
+    let uranusAge = parseFloat((years / 84.01).toFixed(2))
     return uranusAge
   }
 
-  neptuneAge(){
-    let neptuneAge = parseFloat((this.earthAge / 164.79).toFixed(2))
+  neptuneAge(years){
+    let neptuneAge = parseFloat((years / 164.79).toFixed(2))
     return neptuneAge
   }
 
-  plutoAge(){
-    let plutoAge = parseFloat((this.earthAge / 248.59).toFixed(2))
+  plutoAge(years){
+    let plutoAge = parseFloat((years / 248.59).toFixed(2))
     return plutoAge
   }
   
