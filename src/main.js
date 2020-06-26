6df2b8644eb6f018c85aa35330d787068f6fb330
Tyson Lackey <lackeyt90@gmail.com>
2020-06-26 15:54:29 -0700

    create inital UI functions

--- src/main.js
+++ src/main.js
@@ -2,8 +2,23 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import $ from "jquery";
+import { UserDetails } from './userDetails';
+
+function populateOutputAges(user){
+
+}
+
+function populateOutputYearsLeftOrOver(user){
+  
+}
 
 $(document).ready(function() {
+  $("#inputForm").submit(function(event){
+    event.preventDefault()
+    user = new UserDetails(parseInt($("#age").val()), $("input:radio[name=sex]:checked").val(), $("input:radio[name=continent]:checked").val())
+    populateOutputAges(user);
+    populateOutputYearsLeftOrOver(user);
+  })
   //User Interface
 });
 
