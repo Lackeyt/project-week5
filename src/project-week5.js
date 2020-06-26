6f2de037efeb2c8aec348b95d13cca3537827761
Tyson Lackey <lackeyt90@gmail.com>
2020-06-26 10:16:27 -0700

    confirm green light on saturn years test

--- src/project-week5.js
+++ src/project-week5.js
@@ -6,7 +6,8 @@ export class UserPlanetsAge{
     this.mercuryAge = parseFloat((age / .24).toFixed(2));
     this.venusAge = parseFloat((age / .62).toFixed(2));
     this.marsAge = parseFloat((age / 1.88).toFixed(2));
-    this.jupiterAge = parseFloat((age / 11.86).toFixed(2))
+    this.jupiterAge = parseFloat((age / 11.86).toFixed(2));
+    this.saturnAge = parseFloat((age / 29.4).toFixed(2));
   }
   
 }
